d7bad0f7e63ddc8857ea2e1c11dc2d53b390bd3a
SushantDahal <sushandahal@gmail.com>
2024-12-09 17:30:23 +0545

    project setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {  return (
+    <>
+     <div>
+      <h1 className='bg-[black] text-white'>
+        Sushant Dahal
+      </h1>
+     </div>
+    </>
+  )
+}
+
+export default App;
