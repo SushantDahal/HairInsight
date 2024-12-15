a0c40dcfa75575d6ca468f7da3a4915dd51a2c88
SushantDahal <sushandahal246@gmail.com>
2024-12-15 22:19:49 +0545

    .

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,3 +1,13 @@
+// const Login = () => {
+//   return (
+//     <div className="bg-black">
+//       <div>
+//         <h1>Login</h1>
+//       </div>
+//     </div>
+//   );
+// };
+// export default Login;
 const Login = () => {
   return (
     <div className="bg-black">
